262e640d9f5fc264cbb1614adfc7b8740dd443b9
E.W. Peter Jalajas <lists@jalajas.com>
2017-03-23 17:18:42 -0400

    Update LavaLamp.bash

--- LavaLamp.bash
+++ LavaLamp.bash
@@ -5,12 +5,9 @@
 #License: GPL2.0+
 #So dumb.
 
-
-
-clear ; 
 while true ; 
-do ssh-keygen -f /tmp/junk_$RANDOM -N '' -t ecdsa -b 521 | grep -v -e '^G' -e '^Y' -e '^T' -e '^S' ; 
+do clear ; 
+ssh-keygen -f /tmp/junk_$RANDOM -N '' -t ecdsa -b 521 | grep -v -e '^G' -e '^Y' -e '^T' -e '^S' ; 
 echo you just might be eating all your entropy...
 sleep 3 ; 
-clear ; 
 done
